155eb0f932a5ed871516141a323705420aa9388e
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-06-01 17:11:01 +0200

    setup main file

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,13 @@
 #include <intercept.hpp>
 
-#include <boost/algorithm/algorithm.hpp>
-#include <boost/algorithm/string.hpp>
-
 #include <string>
 #include <sstream>
 
-
-#include "../addons/main/script_version.hpp"
+#include "logger.h"
+#include "configuration.h"
 
 using namespace intercept;
-using SQFPar = game_value_parameter;
-
-namespace ba = boost::algorithm;
-
-void prettyDiagLog(std::string msg) {
-    intercept::sqf::diag_log(sqf::text(msg));
-}
-
-void prettyDiagLogInfo(std::string info) {
-    prettyDiagLog("[GRAD] (intercept_template) INFO: " + info);
-}
+using namespace tf47::prism;
 
 int intercept::api_version() {
     return INTERCEPT_SDK_API_VERSION;
@@ -28,31 +15,40 @@ int intercept::api_version() {
 
 void intercept::register_interfaces() {}
 
-void intercept::pre_init() {
-    std::stringstream strVersion;
-    strVersion << "Running (" << MAJOR << "." << MINOR << "." << PATCHLVL << "." << BUILD << ")";
-    prettyDiagLogInfo(strVersion.str());
+bool echelonLoaded = false;
+
+void intercept::pre_init()
+{
+    configuration::load_configuration();
+	configuration::load_mission_config();
+	sqf::set_variable(sqf::mission_namespace(), "TF47Prism", true);
 }
 
-game_value testCommand(game_state& gs, SQFPar right_arg) {
-    if (right_arg.type_enum() == GameDataType::STRING) {
-        prettyDiagLogInfo(ba::to_upper_copy(static_cast<std::string>(right_arg)));
-        return true;
+void intercept::pre_start()
+{
+    if (sqf::get_variable(sqf::mission_namespace(), "TF47Echelon", false))
+    {
+        echelonLoaded = true;
     }
-    else {
-        gs.set_script_error(game_state::game_evaluator::evaluator_error_type::type, "string expected"sv);
-        return false;
-    }
-}
-
-void intercept::pre_start() {
-
-    static auto grad_intercept_template_test_command =
-        client::host::register_sqf_command("gradInterceptTestCommand", "Test Command", testCommand, game_data_type::BOOL, game_data_type::STRING);
+	
+    //static auto grad_intercept_template_test_command
 
 }
 
-void intercept::post_init() {
-
+void intercept::post_init()
+{
+    
 }
+
+void intercept::mission_ended()
+{
+	
+}
+
+void intercept::handle_unload()
+{
+	
+}
+
+
 
