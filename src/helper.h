bea37c5276aefce2007e964ab23ca088b021d452
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-06-19 14:34:49 +0200

    implemented threadsafe vector

--- src/helper.h
+++ src/helper.h
@@ -1,10 +1,5 @@
 #pragma once
 
-#ifndef TF47_PRISM_HELPER_H
-#define TF47_PRISM_HELPER_H
-
-#endif //TF47_PRISM_WHITELIST_H
-
 #include "intercept.hpp"
 
 namespace tf47::prism::helper
@@ -19,11 +14,98 @@ namespace tf47::prism::helper
 		return object_type_return;
 	}
 
-	static std::string get_display_name_vehicle(object& vehicle)
+	static r_string get_display_name_vehicle(object& vehicle)
 	{
-		const std::string vehicleName = get_text(
+		r_string vehicleName = get_text(
 			intercept::sqf::config_entry(intercept::sqf::config_file()) >> "CfgVehicles" >>
 			intercept::sqf::type_of(vehicle) >> "displayName");
 		return vehicleName;
 	}
+
+	template<typename T, typename Y>
+	class thread_safe_vector
+	{
+	private:
+		std::vector<std::pair<T, Y>> data_vector_;
+		std::recursive_mutex lock_;
+	public:
+		thread_safe_vector() {}
+
+		Y* get(T key)
+		{
+			std::lock_guard guard(lock_);
+			if (data_vector_.size() == 0) return nullptr;
+
+			for (size_t i = 0; i < data_vector_.size(); i++)
+			{
+				if (data_vector_[i].first == key)
+				{
+					return &(data_vector_[i].second);
+				}
+			}
+			
+			return nullptr;
+		}
+		void insert(std::pair<T, Y> data, bool overwrite = true)
+		{
+			std::lock_guard guard(lock_);
+			
+			for (auto element : data_vector_)
+			{
+				if (element.first == data.first)
+				{
+					if (overwrite)
+					{
+						element.second = data.second;
+						return;
+					}
+					else
+					{
+						return;
+					}
+				}
+			}
+				
+			data_vector_.push_back(data);
+		}
+		bool exist(T key)
+		{
+			std::lock_guard guard(lock_);
+
+			for (auto element : data_vector_)
+			{
+				if (element.first == key)
+				{
+					return true;
+				}
+			}
+			
+			return false;
+		}
+		void remove(T key)
+		{
+			std::lock_guard guard(lock_);
+
+			for (auto element : data_vector_)
+			{
+				if (element.first == key)
+				{
+					data_vector_.erase(element);
+					return;
+				}
+			}
+		}
+		void clear()
+		{
+			std::lock_guard guard(lock_);
+
+			data_vector_.clear();
+		}
+		std::vector<std::pair<T, Y>> get_vector()
+		{
+			std::lock_guard guard(lock_);
+
+			return data_vector_;
+		}
+	};
 }
